10e6772af6a4dd61642e870d83959a6993469f25
Jennica Llamera <jennica.llamera@gmail.com>
2024-07-19 19:07:41 +1000

    Created routes and controllers for post and photo

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\PhotoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('posts', PostController::class);
+Route::resource('photos', PhotoController::class);
