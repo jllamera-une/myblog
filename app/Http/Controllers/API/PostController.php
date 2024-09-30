330075d02217c2b434470fde699a7bbe6c5a1b30
Jennica Llamera <jllamera@myune.edu.au>
2024-09-30 17:28:57 +1000

    Added API controller for single post

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -16,4 +16,15 @@ class PostController extends Controller
             'message' => 'success'
         ], 200);
     }
+    // Retrieve a single post by ID
+    public function show($id)
+    {
+        // Find the post by ID or return a 404 if not found
+        $post = Post::findOrFail($id);
+
+        return response()->json([
+            'data' => $post,
+            'message' => 'success'
+        ], 200);
+    }
 }
